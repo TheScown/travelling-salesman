7c1436f81b9c0395884502d03ff8b25d8065d0d0
Alex Scown <scowny2000@yahoo.co.uk>
2018-08-18 13:49:35 +0100

    Refactor Graph for better performance
    Remove the MinEdge code as IIRC it didn't work and prevented the cache
    from being used.

--- travelling-salesman/src/main/java/space/scown/travellingsalesman/graph/Graph.java
+++ travelling-salesman/src/main/java/space/scown/travellingsalesman/graph/Graph.java
@@ -6,78 +6,45 @@
 
 package space.scown.travellingsalesman.graph;
 
-import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 public class Graph<T> {
 
+    private final Map<T, Set<Arc<T>>> table = new HashMap<>();
+
     private final Set<T> nodes;
-    protected final Set<Arc<T>> arcs;
 
     public Graph(final Set<T> nodes, final Set<Arc<T>> arcs) {
         this.nodes = nodes;
-        this.arcs = arcs;
+
+        for (final Arc<T> arc : arcs) {
+            if (!table.containsKey(arc.getStart())) {
+                table.put(arc.getStart(), new HashSet<>());
+            }
+
+            table.get(arc.getStart()).add(arc);
+        }
     }
 
     public Set<T> getNodes() {
         return nodes;
     }
 
-    public Set<Arc<T>> getArcs() {
-        return arcs;
-    }
-
-    protected Set<T> successors(final T node) {
-        final HashSet<T> successors = new HashSet<>();
-
-        for (final Arc<T> arc : arcs) {
-            if (arc.isStart(node)) {
-                successors.add(arc.getEnd());
-            }
-        }
-
-        return successors;
-    }
-
     public Set<Arc<T>> startingAt(final T node) {
-        final HashSet<Arc<T>> startingAt = new HashSet<>();
-        for (final Arc<T> arc : arcs) {
-            if (arc.isStart(node)) {
-                startingAt.add(arc);
-            }
-        }
-        return startingAt;
-    }
-
-    public Collection<Arc<T>> endingAt(final T node) {
-        final Collection<Arc<T>> endingAt = new HashSet<>();
-
-        for (final Arc<T> arc : arcs) {
-            if (arc.isEnd(node)) {
-                endingAt.add(arc);
-            }
-        }
-
-        return endingAt;
-    }
-
-    private boolean succeeds(final T n1, final T n2) {
-        return successors(n1).contains(n2);
+        return table.get(node);
     }
 
     public int distance(final T n1, final T n2) throws IllegalArgumentException {
-        if (succeeds(n1, n2)) {
-            final Iterable<Arc<T>> arcs = startingAt(n1);
-            int distance = 0;
-            for (final Arc<T> arc : arcs) {
-                if (arc.isEnd(n2)) {
-                    distance = arc.getWeight();
-                }
-            }
-            return distance;
-        } else {
-            throw new IllegalArgumentException("Nodes not adjacent");
-        }
+        final Optional<Integer> weight = startingAt(n1).stream()
+                .filter(a -> a.getEnd().equals(n2))
+                .findFirst()
+                .map(Arc::getWeight);
+
+        return weight.orElseThrow(() -> new IllegalArgumentException("Nodes not adjacent"));
     }
+
 }
