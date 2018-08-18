403d6a77e882355988265a89edaa9e0fc8276502
Alex Scown <scowny2000@yahoo.co.uk>
2018-08-18 16:22:33 +0100

    Tidy Runner

--- travelling-salesman/src/main/java/space/scown/travellingsalesman/runner/Runner.java
+++ travelling-salesman/src/main/java/space/scown/travellingsalesman/runner/Runner.java
@@ -14,49 +14,49 @@ import java.io.IOException;
 import java.io.PrintStream;
 
 class Runner {
-	
-	//public static final String prefix = "Batch 1/";
 
-	private static final String[] files =
-		{"file12",
-		"file17",
-		"file21",
-		"file26",
-		"file42",
-		"file48",
-		"file58",
-		"file175",
-		"file180",
-		"file535"};
+    //public static final String prefix = "Batch 1/";
 
-	public static void main(final String[] args) throws IOException{
-		final PrintStream writer = System.out;
-		for(final String filename : files){
-			writer.println(filename);
+    private static final String[] files = {
+            "file12",
+            "file17",
+            "file21",
+            "file26",
+            "file42",
+            "file48",
+            "file58",
+            "file175",
+            "file180",
+            "file535"
+    };
 
-			final String[] string = new String[1];
-			string[0] = filename.concat("a.txt");
-			final long greedyStartTime = System.currentTimeMillis();
-			GreedyBestFirstSearch.main(string);
-			final long greedyEndTime = System.currentTimeMillis();
-			writer.println("Greedy: "+((greedyEndTime - greedyStartTime)/1000));
+    public static void main(final String[] args) throws IOException {
+        final PrintStream writer = System.out;
 
-			string[0] = filename.concat("b.txt");
-			final long annealingStartTime = System.currentTimeMillis();
-			SimulatedAnnealing.main(string);
-			final long annealingEndTime = System.currentTimeMillis();
-			writer.println("Annealing: "+((annealingEndTime - annealingStartTime)/1000));
+        for (final String filename : files) {
+            writer.println(filename);
 
-			string[0] = filename.concat("c.txt");
-			final long geneticStartTime = System.currentTimeMillis();
-			GeneticAlgorithm.main(string);
-			final long geneticEndTime = System.currentTimeMillis();
-			writer.println("Genetic: "+((geneticEndTime - geneticStartTime)/1000));
+            final String[] string = new String[1];
+            string[0] = filename.concat("a.txt");
+            final long greedyStartTime = System.currentTimeMillis();
+            GreedyBestFirstSearch.main(string);
+            final long greedyEndTime = System.currentTimeMillis();
+            writer.println("Greedy: " + ((greedyEndTime - greedyStartTime) / 1000));
 
-			writer.println();
-		}
-		writer.close();
+            string[0] = filename.concat("b.txt");
+            final long annealingStartTime = System.currentTimeMillis();
+            SimulatedAnnealing.main(string);
+            final long annealingEndTime = System.currentTimeMillis();
+            writer.println("Annealing: " + ((annealingEndTime - annealingStartTime) / 1000));
 
-	}
+            string[0] = filename.concat("c.txt");
+            final long geneticStartTime = System.currentTimeMillis();
+            GeneticAlgorithm.main(string);
+            final long geneticEndTime = System.currentTimeMillis();
+            writer.println("Genetic: " + ((geneticEndTime - geneticStartTime) / 1000));
+
+            writer.println();
+        }
+    }
 
 }
